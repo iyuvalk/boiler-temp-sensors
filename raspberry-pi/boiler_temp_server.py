55cd5d25f936a907dd7c9cad51509166bf3f6678
Yuval Khalifa <yuval@coralogix.com>
2025-02-18 16:43:13 +0200

    Added function for calculating the average

--- raspberry-pi/boiler_temp_server.py
+++ raspberry-pi/boiler_temp_server.py
@@ -3,6 +3,18 @@
 import serial
 import time
 
+
+def trimmed_average(temps):
+    if len(temps) < 7:
+        return min(temps) if temps else None  # Return lowest value or None if the list is empty
+
+    sorted_temps = sorted(temps)  # Sort the list
+    trimmed_temps = sorted_temps[3:-3]  # Remove the top and bottom 3
+    average = sum(trimmed_temps) / len(trimmed_temps)  # Calculate the average
+
+    return average
+
+
 dev_id = "/dev/ttyUSB0"
 s = serial.Serial(dev_id, 9600, timeout=1)
 time.sleep(2)
